9c74c6611005d9cff47d62b8d67d7158c83cd54c
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-31 13:47:25 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,13 @@
+//Class Friday May 10 2024------------------------------------------------------------------------------------------------------------------
+variable "subscription_id"{
+  type=string
+}
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
