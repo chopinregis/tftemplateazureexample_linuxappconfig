a69523ece33c9662ff584bee1b5e84c087f18c29
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-31 13:44:44 -0400

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "azure_linux_app"{
+  value = local.linux_app_list
+}
+output "linux_app_names"{
+  value = [for app in local.linux_app_list:app.name]
+}
+output "linux_app_os"{
+  value = [for app in local.linux_app_list:app.os_type]
+}
+output "linux_app_sku"{
+  value = [for app in local.linux_app_list:app.sku_name]
+}
